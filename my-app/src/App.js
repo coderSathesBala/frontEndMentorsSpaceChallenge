05a988d90dcb51ce87c6db1bcbd3366b663f5d30
coderSathesBala <sathes352@gmail.com>
2021-12-18 13:45:05 -0500

    addReact

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SO YOU WANT TO TRAVEL TO</h1>
+      <h1>SPACE</h1>
+      <h1>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, 
+        and relax because we’ll give you a truly out of this world experience!</h1>
+    </div>
+  );
+}
+
+export default App;
