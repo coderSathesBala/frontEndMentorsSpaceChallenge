ba7d927e08f09a5994c2997293c368e9ed632981
coderSathesBala <sathes352@gmail.com>
2021-12-23 13:17:14 -0500

    startedOnNavBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 class Navbar extends React.Component {
     render() {
       return (
+        <div>
+
       <ul>
         <li>00 HOME</li>
         <li>01 DESTINATION</li>
         <li>02 CREW</li>
         <li>03 TECHNOLOGY</li>
       </ul>
+
+<NavLink to="/" >
+    Home
+</NavLink>
+<NavLink to="/about" >
+    About
+</NavLink>
+<NavLink to="/contact" >
+    Contact
+</NavLink>
+<NavLink to="/signin" >
+    Sign In
+</NavLink>
+        </div>
       )
     }
   }
