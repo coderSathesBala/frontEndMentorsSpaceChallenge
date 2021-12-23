e969a497bb3057057dc5450f28e2fbfe8d29a28b
coderSathesBala <sathes352@gmail.com>
2021-12-23 12:57:56 -0500

    addTechnologyState

--- src/components/destinationPage.js
+++ src/components/destinationPage.js
@@ -1,14 +1,67 @@
 import React from 'react';
 
 class Destination extends React.Component {
+
+  state = {
+    title: 'Moon',
+    description: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+    distance: '384,400 km',
+    travel: '3 days'
+  }
+
+  moon() {
+    return this.setState({
+      title: 'Moon',
+      description: "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+      distance: '384,400 km',
+      travel: '3 days'
+    })
+  }
+
+  titan() {
+    return this.setState({
+      title: 'Titan',
+      description: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
+      distance: '1.6 bil. km',
+      travel: '7 years'
+    })
+  }
+
+  mars() {
+    return this.setState({
+      title: 'Mars',
+      description: "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+      distance: '225 mil. km',
+      travel: '9 months'
+    })
+  }
+
+  europa() {
+    return this.setState({
+      title: 'Europa',
+      description: "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.", 
+      distance: '628 mil. km',
+      travel: '3 years'
+    })
+  }
+
+
     render() {
       return (
       <div>
         <h1>Pick Your Destination</h1>;
-        <h1>Moon</h1>
-        <h1>Mars</h1>
-        <h1>Europa</h1>
-        <h1>Titan</h1>
+        <ul>
+          <li onClick={this.moon.bind(this)}>MOON</li>
+          <li onClick={this.mars.bind(this)}>MARS</li>
+          <li onClick={this.europa.bind(this)}>EUROPA</li>
+          <li onClick={this.titan.bind(this)}>TITAN</li>
+        </ul>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.description}</p>
+        <h1>AVG. DISTANCE</h1>
+        <h1>EST. TRAVEL TIME</h1>
+        <h1>{this.state.distance}</h1>
+        <h1>{this.state.travel}</h1>
       </div>
         )
     }
--- src/components/technologyPage.js
+++ src/components/technologyPage.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
 class Technology extends React.Component {
+
+  state = {
+    title: 'LAUNCH VEHICLE',
+    description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+  }
+
+  one() {
+    return this.setState({
+      title: 'LAUNCH VEHICLE',
+      description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+    })
+  }
+
+  two() {
+    return this.setState({
+      title: 'SPACEPORT',
+      description: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+    })
+  }
+
+  three() {
+    return this.setState({
+      title: 'SPACE CAPSULE',
+      description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+    })
+  }
+
     render() {
       return (
-      <h1>SPACE LAUNCH 101</h1>
+      <div>
+        <h1>SPACE LAUNCH 101</h1>
+        <button onClick={this.one.bind(this)}>1</button>
+        <button onClick={this.two.bind(this)}>2</button>
+        <button onClick={this.three.bind(this)}>3</button>
+        <h2>THE TERMINOLOGY...</h2>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.description}</p>
+      </div>
       )
     }
   }
