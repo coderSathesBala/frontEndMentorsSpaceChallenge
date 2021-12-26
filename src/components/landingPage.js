7a85571ca16212cc91c66fe42a11aea3f9cdabed
coderSathesBala <sathes352@gmail.com>
2021-12-26 18:39:30 -0500

    AddBackgroundLandingPage

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Typography, Box} from '@material-ui/core'
+import Image from '../images/homeImages/background-home-desktop.jpg'
 
 function Landing() {
-
       return (
-        <Box>
-        <Typography variant="h2">SO, YOU WANT TO TRAVEL TO </Typography>;
+        <Box  style={{
+                        backgroundImage: `url(${Image})`,
+                        backgroundSize: "cover",
+                        height: "100vh",
+                        color: "#f5f5f5"
+        }}>
+        <Typography variant="h2">SO, YOU WANT TO TRAVEL TO </Typography>
         <Typography variant="h2">SPACE</Typography>
-        <Typography variant="p">Let's face it: if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back,
+        <Typography>Let's face it: if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back,
           and relax because we'll give you a truly out of this world experience!
         </Typography>
       </Box>
