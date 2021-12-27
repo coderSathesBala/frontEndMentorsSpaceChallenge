0bb01e0fb43daba2c98d9af6f186a82a7ee8bd48
coderSathesBala <sathes352@gmail.com>
2021-12-27 12:50:44 -0500

    addedAllPicturesDesktop

--- src/components/technologyPage.js
+++ src/components/technologyPage.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import {Typography, Box, Button} from '@material-ui/core';
-import Image from '../images/technologyImages/background-technology-desktop.jpg';
+import {Typography, Box, Button, Paper} from '@material-ui/core';
+import Image from '../images/technologyImages/background-technology-desktop.jpg'
+import one from '../images/technologyImages/image-launch-vehicle-portrait.jpg'
+import two from '../images/technologyImages/image-space-capsule-portrait.jpg'
+import three from '../images/technologyImages/image-spaceport-portrait.jpg'
 
 class Technology extends React.Component {
 
   state = {
     title: 'LAUNCH VEHICLE',
-    description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+    description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+    image: '/static/media/image-launch-vehicle-portrait.0c758955a178bdcc3230.jpg'
   }
 
   one() {
     return this.setState({
       title: 'LAUNCH VEHICLE',
-      description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+      description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+      image: '/static/media/image-launch-vehicle-portrait.0c758955a178bdcc3230.jpg'
     })
   }
 
   two() {
     return this.setState({
       title: 'SPACEPORT',
-      description: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+      description: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+      image: '/static/media/image-space-capsule-portrait.dd0ac138afef98c1666a.jpg'
     })
   }
 
   three() {
     return this.setState({
       title: 'SPACE CAPSULE',
-      description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+      description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+      image: '/static/media/image-spaceport-portrait.85523db5de13057f36ea.jpg'
     })
   }
 
@@ -39,12 +46,13 @@ class Technology extends React.Component {
           color: "#f5f5f5"
         }}>
         <Typography variant="h1">SPACE LAUNCH 101</Typography>
-        <Button onClick={this.one.bind(this)}>1</Button>
-        <Button onClick={this.two.bind(this)}>2</Button>
-        <Button onClick={this.three.bind(this)}>3</Button>
+        <Button style={{backgroundColor: "#21b6ae"}} onClick={this.one.bind(this)}>1</Button>
+        <Button style={{backgroundColor: "#21b6ae"}} onClick={this.two.bind(this)}>2</Button>
+        <Button style={{backgroundColor: "#21b6ae"}} onClick={this.three.bind(this)}>3</Button>
         <Typography variant="h2">THE TERMINOLOGY...</Typography>
         <Typography variant="h1">{this.state.title}</Typography>
         <Typography variant="p">{this.state.description}</Typography>
+        <Paper variant="outlined"><img src={this.state.image}/></Paper>
       </Box>
       )
     }
