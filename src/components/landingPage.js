cd928dc972628e19eb84cdaff4bb1845f2360c5d
coderSathesBala <sathes352@gmail.com>
2021-12-29 10:18:36 -0500

    StylingLandingPage

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -4,19 +4,31 @@ import Image from '../images/homeImages/background-home-desktop.jpg'
 
 function Landing() {
       return (
-      <Box  style={{
+      <Box  display="grid" direction="column" gridTemplateColumns="repeat(3, 1fr)" 
+            sx={{
             backgroundImage: `url(${Image})`,
             backgroundSize: "cover",
             height: "100vh",
             color: "#f5f5f5"
         }}>
+        
+        <Box style={{paddingTop: '21rem', paddingLeft: '12rem'}}>
         <Typography variant="h2" style={{ fontSize: '1.5rem'}}>SO, YOU WANT TO TRAVEL TO </Typography>
-        <Typography variant="h1" style={{ fontSize: '10rem' }}>SPACE</Typography>
-        <Typography>Let's face it: if you want to go to space, you might as well</Typography> 
+        <Typography variant="h1" style={{ fontSize: '10rem', paddingTop: '1rem' }}>SPACE</Typography>
+        <Typography style={{paddingTop: '1.5rem'}}>Let's face it: if you want to go to space, you might as well</Typography> 
         <Typography>genuinely go to outer space and not hover not hover kind of on</Typography> 
         <Typography>the edge of it. Well sit back, and relax because we'll give you</Typography> 
         <Typography>a truly out of this world experience!</Typography>
-        <Button style={{color: 'white', fontSize: '1rem' }}> Explore</Button>
+        </Box>
+        <Box></Box>  
+          <Box style={{paddingTop: '27rem'}}><Button 
+                  style={{
+                    color: 'black', 
+                    fontSize: '1.4rem', 
+                    backgroundColor: "white",
+                    padding: '7.5rem 6rem',
+                    borderRadius: '50%'
+                    }}> Explore</Button></Box>
       </Box>
         )
 }
